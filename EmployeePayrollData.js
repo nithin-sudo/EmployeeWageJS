a529170c2413bf597d829fd969cced976fe11bd5
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-08-06 23:48:55 +0530

    Added: Regex and try catch in case of error.

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -19,7 +19,49 @@ class EmployeePayrollData{
             this._name = name;
         else throw 'Name is Incorrect!';    
     }
-
+    get id(){ return this._id}
+    set id(id){ 
+        let idRegex = RegExp("^[1-9]{1}[0-9]{0,}$")
+        if (idRegex.test(id)) {
+            this._id = id
+        }
+        else{
+            throw "Id is incorrect!"
+        }
+    }
+    
+    get salary(){return this._salary}
+    set salary(salary){
+        let salaryRegex= RegExp("^[1-9]{1}[0-9]{0,}$")
+        if (salaryRegex.test(salary)) {
+            this._salary = salary
+        }
+        else{
+            throw "Salary is incorrect!"
+        }
+    }
+   
+    
+    get gender(){return this._gender}
+    set gender(gender){
+        let genderRegex= RegExp("^[MFmf]{1}$")
+        if (genderRegex.test(gender)) {
+            this._gender = gender
+        }
+        else{
+            throw "Gender is incorrect!"
+        }
+    }
+    
+    get startDate(){return this._startDate}
+    set startDate(startDate){
+        if (startDate <= new Date() ) {
+            this._startDate = startDate
+        }
+        else{
+            throw "date  is incorrect!"
+        }
+    }
     //method
     toString(){
         const options = { year: 'numeric', month: 'long', day: 'numeric'};
@@ -33,6 +75,8 @@ class EmployeePayrollData{
 
 let employeePayrollData = new EmployeePayrollData(1,"Mark",30000);
 console.log(employeePayrollData.toString());
+let newEmployeePayrollData = new EmployeePayrollData(1,"Terrisa", 30000,"F",new Date());
+console.log(newEmployeePayrollData.toString());
 employeePayrollData.id = 0;
 try{
 employeePayrollData.name = "jeff";
@@ -40,5 +84,31 @@ console.log(employeePayrollData.toString());
 }catch(e){
     console.error(e);
 }
-let newEmployeePayrollData = new EmployeePayrollData(1,"Terrisa", 30000,"F",new Date());
-console.log(newEmployeePayrollData.toString());
+
+try {
+    newEmployeePayrollData.id = -5
+    console.log(newEmployeePayrollData.toString());
+} catch (error) {
+    console.error(error);
+}
+
+try {
+    newEmployeePayrollData.salary = -90  
+    console.log(newEmployeePayrollData.toString());
+} catch (error) {
+    console.error(error);
+}
+
+try {
+    newEmployeePayrollData.gender = "NA" 
+    console.log(newEmployeePayrollData.toString());
+} catch (error) {
+    console.error(error);
+}
+
+try {
+    newEmployeePayrollData.startDate = "2021-09-06T50:44:41.979Z"  
+    console.log(newEmployeePayrollData.toString());
+} catch (error) {
+    console.error(error);
+}
